c8ff5fef4ba99ccc2a1f12c7cc982ad96082a0e2
ngnlesn33 <son.nl215131@gmail.com>
2023-12-14 17:18:24 +0700

    Install JavaFx, SceneBuilder + Fix glitch Wsl2 gui app

---
+++ AimsProject/src/hust/soict/hedspi/javafx/GUIProject/src/main/java/org/example/guiproject/controllers/PainterController.java
@@ -0,0 +1,24 @@
+package org.example.guiproject.controllers;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class PainterController {
+
+    @FXML
+    private VBox drawingAreaPane;
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+
+    }
+
+}
